5d45a80a6ce0e0f999cd4ea42c19f97d6db5760c
Seibe TAKAHASHI <t@seibe.jp>
2016-09-11 03:01:49 +0900

    Update API document v0.3.3

--- doc/hierarchy.js
+++ doc/hierarchy.js
@@ -1,8 +1,6 @@
 var hierarchy =
 [
-    [ "GameCanvas.Event", "class_game_canvas_1_1_event.html", null ],
     [ "GameCanvas.GameBase", "class_game_canvas_1_1_game_base.html", null ],
-    [ "GameCanvas.MessageEvent", "class_game_canvas_1_1_message_event.html", null ],
     [ "GameCanvas.SerializableDictionary< TKey, TValue >", "class_game_canvas_1_1_serializable_dictionary.html", null ],
     [ "GameCanvas.SerializableDictionary< string, string >", "class_game_canvas_1_1_serializable_dictionary.html", null ],
     [ "GameCanvas.SingletonMonoBehaviour< T >", "class_game_canvas_1_1_singleton_mono_behaviour.html", null ],
