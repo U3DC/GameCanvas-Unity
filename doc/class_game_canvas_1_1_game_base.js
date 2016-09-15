132a42267928112347f007d0ffe9cae33f99f556
Seibe TAKAHASHI <t@seibe.jp>
2016-09-16 01:33:28 +0900

    Update API document v0.3.4

--- doc/class_game_canvas_1_1_game_base.js
+++ doc/class_game_canvas_1_1_game_base.js
@@ -2,8 +2,6 @@ var class_game_canvas_1_1_game_base =
 [
     [ "Calc", "class_game_canvas_1_1_game_base.html#acce15a0f14259d11a2679c750ebbe5d4", null ],
     [ "Draw", "class_game_canvas_1_1_game_base.html#a5f02608e5321ccf91243fc6b35c33983", null ],
-    [ "Final", "class_game_canvas_1_1_game_base.html#aec4cb5fe7b7ab3755a2de959a7743761", null ],
-    [ "Pause", "class_game_canvas_1_1_game_base.html#afb66567ddcc832c2ba424054bd42486f", null ],
     [ "Start", "class_game_canvas_1_1_game_base.html#ade59be4069a3e7e326915bb0a2e55cdb", null ],
     [ "gc", "class_game_canvas_1_1_game_base.html#a8f8c173b93b33515b894f72e2662bf17", null ]
 ];
